f4fe61107aa91703a1b9cd5cfc3a17bb517ce204
isauljosue <56583387+isauljosue@users.noreply.github.com>
2022-11-13 19:23:57 -0400

    grammatical correction

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -23,7 +23,7 @@ const Module = () => {
                 <SectionText>
                     <SmallText>Hello, my name is Isaúl. I am a designer and a frontend developer who enjoys exploring the digital and making fun engaging interactions. I like building products for the web, as some of my main interests are in the evolution of the web and mixed realities.</SmallText>
                     <br />
-                    <SmallText>I am open for work! Got a position for a frontend developer, need a web site for your business, or have an idea for a collaboration? Send me an email.</SmallText>
+                    <SmallText>I am open for work! Got a position for a frontend developer? Need a web site for your business? Or, Have an idea for a collaboration? Send me an email.</SmallText>
                     <br />
                     <SmallText>Let's build something.</SmallText>
                     <br />
